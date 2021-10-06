dc9a3dde0211ebc38530c4f54f6ecfc29f5fffa9
saulpaiva <saulpaiva@gmail.com>
2021-10-06 11:33:10 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/12.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/12.py
@@ -1,2 +1,14 @@
 '''Strings in Python'''
 
+'''strings in python beggins with the "0" index '''
+
+# fruit = 'pineapple'
+# letter = fruit[0]
+# # print('First letter:', letter)
+# x = len(fruit)
+# print('length:', x)
+
+'''looping through strings'''
+
+# fruit = 'banana'
+# 4:51
