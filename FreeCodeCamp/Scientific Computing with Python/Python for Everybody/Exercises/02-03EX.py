b3bc7a2391492935e7e981156185e476e93d8c70
saulpaiva <saulpaiva@gmail.com>
2021-10-07 12:00:19 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/Exercises/02-03EX.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/Exercises/02-03EX.py
@@ -2,37 +2,19 @@
 
 '''Write a program to prompt the user for hours and rate per hour to compute gross pay'''
 
-
-
 while True :
-    hours = input('How many hours do you work in a week? ')
-    if hours[0] == '#' :
-        continue
-    if hours == 'done' :
-        break
-    if hours == 'Done' :
-        break
-    if hours == 'DONE' :
-        break
-    if hours < 0 :
-        break
-    try :
-        float(hours)
-    except:
-        print('You have to digit a number!')
-    rate = input('Hou many are you payed for hour worked?')
-    if rate[0] == '#' :
-        continue
-    if rate == 'done' :
-        break
-    if rate == 'Done' :
-        break
-    if rate == 'DONE' :
-        break
-    if rate < 0 :
-        break
-    try :
-        float(hours)
-    except:
-        print('You have to digit a number!')
+    # total hours worked within a week
+    hours = float(input('How many hours do you work in a week? '))
+    # rate of money per hour worked
+    rate = float(input('Hou many are you payed for hour worked?'))
+    # print the gross pay in a week and a month of work
+    print('The gross pay in a week of work is:', hours*rate, '\n', 'And for approximately a month of work is: ', hours*rate*4)
+    break
 print('Done!')
+
+''''this simple problem can be more complex and complete!'''
+
+# try:
+#     print('Try this')
+# except:
+#     print('fail')
