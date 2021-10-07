0f5fea87bdc9773eb8e664bcce404d197102602f
saulpaiva <saulpaiva@gmail.com>
2021-10-07 09:48:29 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/12.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/12.py
@@ -4,11 +4,43 @@
 
 # fruit = 'pineapple'
 # letter = fruit[0]
-# # print('First letter:', letter)
+# print('First letter:', letter)
 # x = len(fruit)
 # print('length:', x)
 
 '''looping through strings'''
 
 # fruit = 'banana'
-# 4:51
+# index = 0
+# while index < len(fruit):
+#     letter = fruit[index]
+#     print(index, letter)
+#     index += 1
+
+'''other way: using "for" loop'''
+
+# fruit = 'banana'
+# for letter in fruit :
+#     print(letter)
+
+'''printing letter and index'''
+
+# fruit = 'banana'
+# index = 0
+# for letter in fruit :
+#     print(index, letter)
+#     index += 1
+
+'''looping and counting'''
+
+# word = 'banana'
+# count = 0
+# for letter in word :
+#     if letter == 'a' :
+#         count += 1
+# print('count:', count)
+
+'''question'''
+
+# for n in 'banana' :
+#     print(n)
