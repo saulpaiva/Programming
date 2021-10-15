672bce7ca86ecd97b388fa2f1e3509bb130468b8
saulpaiva <saulpaiva@gmail.com>
2021-10-15 15:15:26 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/Exercises/05-01ex.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/Exercises/05-01ex.py
@@ -1,18 +1,31 @@
 '''Write a program which repeatedly reads numbers until the user enters "done". Once "done" is entered, print out the total, count, and average of the numbers. If the user enters anything other than a number, detect their mistake using "try" and "except" and print an error message and skip to the next number.'''
 
+# global variables
+s = 0
+i = 0
+
+# summary function
+def summary():
+    try:
+        a = s/i
+    except:
+        a = None
+    print('total =', s)
+    print('count =', i)
+    print('average =', a)
+
+# iteration
 while True:
-    s = 0
     try:
         n = input('Enter a number: ')
         if n[0] == '#' :
             continue
-        if n == 'done' :
-            break
-        if n == 'Done' :
-            break
-        if n == 'DONE' :
+        if n == 'done' or n ==  'Done' or n == 'DONE' :
+            summary()
             break
         n = int(n)
+        # n = float(n)
         s += n
+        i += 1
     except:
         print('Invalid input')
