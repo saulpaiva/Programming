cce511c34540f2c1c8dd1ef8e1a03938f17d942a
saulpaiva <saulpaiva@gmail.com>
2021-10-15 17:10:48 -0300

    update

---
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/13-intermediate_strings.py
@@ -0,0 +1,55 @@
+'''Intermediate Strings'''
+
+'''slicing strings'''
+
+# s = 'Monty Python'
+# # s sub 0 through 12...
+# # colon ":" operator
+# print(s[0:12])
+# print(s[0:1])
+# print(s[6:100])
+# print(s[:])
+
+'''string concatenation'''
+
+# a = 'Hello'
+# b = 'There'
+# c = a + b
+# print(c)
+# d = a + ' ' + b
+# print(d)
+
+# print(a,b)
+
+'''using "in" as a logical operator'''
+
+fruit = 'banana'
+
+# # in python terminal:
+# 'n' in fruit
+# 'm' in fruit
+# 'nan' in fruit
+
+# if 'a' in fruit :
+#     print('Found it!')
+
+'''string comparison'''
+
+word = 'Banana'
+
+if word == 'banana' :
+    print('All right, bananas.')
+
+# or:
+
+if word < 'banana' :
+    print('Your word, ' + word + ', comes before banana.')
+elif word > 'banana' :
+    print('Your word, ' + word + ', comes after banana.')
+else :
+    print('All right, bananas.')
+# upper and lower case makes difference
+# in general, upper case comes before any lower case
+
+'''string library'''
+
