08652e088a54255b440e88c93521d4ccdc12e9a6
saulpaiva <saulpaiva@gmail.com>
2021-10-19 15:04:47 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/13-intermediate_strings.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/13-intermediate_strings.py
@@ -23,7 +23,7 @@
 
 '''using "in" as a logical operator'''
 
-fruit = 'banana'
+# fruit = 'banana'
 
 # # in python terminal:
 # 'n' in fruit
@@ -35,21 +35,92 @@ fruit = 'banana'
 
 '''string comparison'''
 
-word = 'Banana'
+# word = 'Banana'
 
-if word == 'banana' :
-    print('All right, bananas.')
+# if word == 'banana' :
+#     print('All right, bananas.')
 
-# or:
+# # or:
 
-if word < 'banana' :
-    print('Your word, ' + word + ', comes before banana.')
-elif word > 'banana' :
-    print('Your word, ' + word + ', comes after banana.')
-else :
-    print('All right, bananas.')
-# upper and lower case makes difference
-# in general, upper case comes before any lower case
+# if word < 'banana' :
+#     print('Your word, ' + word + ', comes before banana.')
+# elif word > 'banana' :
+#     print('Your word, ' + word + ', comes after banana.')
+# else :
+#     print('All right, bananas.')
+# # upper and lower case makes difference
+# # in general, upper case comes before any lower case
 
 '''string library'''
 
+# greet = 'Hello Bob'
+# zap = greet.lower()
+# print(zap)
+# print(greet)
+# print('Hi There'.lower())
+
+'''string-methods'''
+
+# stuff = 'Hello World'
+# type(stuff)
+# dir(stuff)
+
+'''searching a string'''
+
+# fruit = 'banana'
+# # position of some string with ".find"
+# pos = fruit.find('na')
+# print(pos)
+# aa = fruit.find('z')
+# print(aa)
+
+'''making everything UPPER CASE and lower case'''
+
+# greet = 'Hello Bob'
+# nnn = greet.upper()
+# print(nnn)
+# www = greet.lower()
+# print(www)
+
+'''search and replace'''
+
+# greet = "Hello Bob"
+# nstr = greet.replace('Bob','Jane')
+# nstr2 = greet.replace('o','X')
+# print(greet)
+# print(nstr)
+# print(nstr2)
+
+'''stripping whitespace'''
+
+# greet = '   Hello Bob   '
+# l = greet.lstrip()
+# r = greet.rstrip()
+# both = greet.strip()
+
+# print('.' + l + '.')
+# print('.' + r + '.')
+# print('.' + both + '.')
+# # can be tab, newline, whitespaces...
+
+'''prefixes'''
+
+# line = 'Please have a nice day'
+# Please = line.startswith('Please')
+# p = line.startswith('p')
+# print(Please)
+# print(p)
+
+'''parsing and extracting'''
+
+# data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+# atpos = data.find('@')
+# print(atpos)
+# sppos = data.find(' ',atpos)
+# print(sppos)
+# host = data[atpos+1 : sppos]
+# print('> ' + host + ' <')
+
+'''two kinds of strings'''
+
+# unicode operation is still a string on Python 3.5.1 (in Python 3, all strings are unicode)
---
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/Exercises/06-05.py
@@ -0,0 +1,5 @@
+'''Take the following Python code that stores a string:
+
+str = 'X-DSPAM-Confidence: 0.8475 ' 
+
+Use "find" and string slicing to extract the portion of the string after the colon character and then use the "float" function to convert the extracted string into a floating point number. '''
