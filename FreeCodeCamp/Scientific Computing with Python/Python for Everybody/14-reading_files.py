482402b53f09ded27f72ede261b228388d6379c5
saulpaiva <saulpaiva@gmail.com>
2021-10-20 14:49:12 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/14-reading_files.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/14-reading_files.py
@@ -2,3 +2,16 @@
 
 '''using open'''
 
+# # examples:
+# handle = open(filename, mode)
+
+# fhand = open('nbox.txt', 'r')
+
+'''the new line character'''
+
+stuff = 'Hello\nWorld!'
+print(stuff)
+stuff = 'X\nY'
+print(stuff)
+print(len(stuff))
+
