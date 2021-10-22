752ad078c2cd50d1428c8715d5ebfb06571cc865
saulpaiva <saulpaiva@gmail.com>
2021-10-22 14:26:42 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/16-python_lists.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/16-python_lists.py
@@ -1,4 +1,74 @@
 '''what is not a "collection" '''
 
-# file: Scientific Computing with Python\Python for Everybody\mbox.txt
+# "Most of our variables have one value in them - when we put a new value in the variable, the old value in overwritten"
+
+'''a list is a kind of collection'''
+
+# friends = ['Joseph','Glenn','Sally']
+
+# carryon = ['socks','shirt','perfume']
+
+'''list constants'''
+
+# print([1, [5, 6], 7])
+
+'''we already use lists!'''
+
+# for i in [5, 4, 3, 2, 1]:
+#     print(i)
+# print('Blastoff!')
+
+'''lists and definite loops - best pals'''
+
+
+# friends = ['Joseph','Glenn','Sally']
+# for friend in friends:
+#     print('Happy New Year:', x)
+# print('Done!')
+
+# z = ['Joseph','Glenn','Sally']
+# for x in z:
+#     print('Happy New Year:', x)
+# print('Done!')
+
+'''looking inside lists'''
+
+# friends = ['Joseph','Glenn','Sally']
+# print(friends[0])
+
+'''lists are mutable'''
+# and strings are immutable
+
+# # gives TypeError!
+# fruit = 'Banana'
+# fruit[0] = 'b'
+# print(fruit)
+
+# # it works!
+# lotto = [2, 14, 26, 41, 63]
+# print(lotto)
+# lotto[2] = 30.0
+# print(lotto)
+
+'''how long is a list'''
+
+# greet = 'Hello Bob'
+# print(len(greet))
+
+# x = [1, 2, 'Jon', 100]
+# print(len(x))
+
+'''using the "range" function'''
+
+# print(range(4))
+
+# friends = ['Joseph','Glenn','Sally']
+# print(range(len(friends)))
+
+'''a tale of two loops...'''
+
+friends = ['Joseph','Glenn','Sally']
+
+for friend in friends :
+    print('Happy New Year:', friend)
 
