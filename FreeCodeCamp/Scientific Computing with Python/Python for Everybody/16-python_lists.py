114cc6fb62712b4010a0adda7b0784dd27bb7b8c
saulpaiva <saulpaiva@gmail.com>
2021-10-22 15:45:46 -0300

    update

--- FreeCodeCamp/Scientific Computing with Python/Python for Everybody/16-python_lists.py
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/16-python_lists.py
@@ -67,8 +67,11 @@
 
 '''a tale of two loops...'''
 
-friends = ['Joseph','Glenn','Sally']
+# friends = ['Joseph','Glenn','Sally']
 
-for friend in friends :
-    print('Happy New Year:', friend)
+# for friend in friends :
+#     print('Happy New Year:', friend)
 
+# for i in range(len(friends)) :
+#     friend = friends[i]
+#     print('Happy New Year:', friend)
---
+++ FreeCodeCamp/Scientific Computing with Python/Python for Everybody/17-working_with_lists.py
@@ -0,0 +1,68 @@
+'''concatenating list using +'''
+
+# a = [1, 2, 3]
+# b = [4, 5, 6]
+# c = a + b
+# print(a, b)
+# print(c)
+
+'''lists can be sliced using ":" '''
+
+# # remember: just like in strings, the second number is "up to but not including"
+# t = [9, 41, 12, 3, 74, 15]
+# print(t)
+# print(t[1:3])
+# print(t[:4])
+# print(t[3:])
+# print(t[:])
+
+'''list methods'''
+
+# x = list()
+# print(type(x))
+# # documentation: "dir()"
+# print(dir(x))
+
+'''building a list from scratch'''
+
+# # make an empty list
+# stuff = list()
+# print(stuff)
+
+# stuff.append('book')
+# stuff.append(99)
+# print(stuff)
+
+# stuff.append('cookie')
+# print(stuff)
+
+'''is something in a list?'''
+
+# some = [1, 9, 21, 10, 16]
+# x = 20
+# print('Is', x, 'in the list?\nR:', x in some)
+
+'''lists are in order'''
+
+# friends = ['Joseph', 'Glenn', 'Sally']
+# print(friends)
+# # sort alphabetcally
+# friends.sort()
+# print(friends)
+
+'''built-in functions and lists'''
+
+nums = [3, 41, 12, 9, 74, 15]
+
+# print('Len',len(nums))
+# print('Max',max(nums))
+# print('Min',min(nums))
+# print('Sum',sum(nums))
+# print('Avg',sum(nums)/len(nums))
+
+'''average examples'''
+
+total = 0
+count = 0
+while True :
+    inp = input('Enter a number: ')
