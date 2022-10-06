e6828dc1daec9fa0802fb6373bebe0c1d271c285
saulpaiva <saulpaiva@gmail.com>
2022-10-06 20:47:01 -0300

    update

---
+++ DIO/Data-Science Unimed/Python para Cientista de Dados/tios_de_dados.py
@@ -0,0 +1,22 @@
+## Tipos de dados em Python
+
+# int
+print(1 + 10)
+
+# float
+print(1.5 + 3)
+
+# bool
+print(True)
+print(False)
+
+# string
+print("Python")
+
+# Através do tipo, é possível saber o quanto de memória alocado para cada elemento
+
+# # Classes 
+# int()
+# float()
+# bool()
+# str()
