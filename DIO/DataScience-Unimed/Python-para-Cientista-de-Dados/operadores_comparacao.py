584e264bf5f78149977a28777e0545da6399badc
saulpaiva <saulpaiva@gmail.com>
2022-10-28 00:01:30 -0300

    update

---
+++ DIO/DataScience-Unimed/Python-para-Cientista-de-Dados/operadores_comparacao.py
@@ -0,0 +1,11 @@
+saldo = 200
+saque = 200
+
+# exemplo de comparações
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
+
