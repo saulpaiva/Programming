448e93122290ad016ade8baff22960b977d2704a
saulpaiva <saulpaiva@gmail.com>
2022-10-27 22:37:03 -0300

    update

---
+++ DIO/DataScience-Unimed/Python-para-Cientista-de-Dados/operadores_aritimeticos.py
@@ -0,0 +1,23 @@
+produto_1 = 20
+produto_2 = 10
+
+#soma
+print(produto_1 + produto_2)
+
+#subtração
+print(produto_1 - produto_2)
+
+#divisão
+print(produto_1 / produto_2)
+
+#divisão inteira
+print(produto_1 // produto_2)
+
+#multiplicação
+print(produto_1 * produto_2)
+
+#módulo (resto da divisão)
+print(produto_1 % produto_2)
+
+#exponenciação
+print(produto_1 ** produto_2)
