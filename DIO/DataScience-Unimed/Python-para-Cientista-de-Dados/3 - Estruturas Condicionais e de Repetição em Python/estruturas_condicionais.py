1f41627cfe32ad71837aec8a3fad89f21cc04788
saulpaiva <saulpaiva@gmail.com>
2022-10-28 23:21:00 -0300

    update

--- DIO/DataScience-Unimed/Python-para-Cientista-de-Dados/3 - Estruturas Condicionais e de Repetição em Python/estruturas_condicionais.py
+++ DIO/DataScience-Unimed/Python-para-Cientista-de-Dados/3 - Estruturas Condicionais e de Repetição em Python/estruturas_condicionais.py
@@ -1,2 +1,17 @@
 # if/if-else/elif
 
+# exemplo 1
+
+saldo = 2000.0
+saque = float(input("Informe o valor do saque: "))
+
+if saldo >= saque:
+    print("Realizando o saque!")
+
+# if saldo < saque:
+#     print("Saldo insuficiente!")
+
+else:
+    print("Saldo insuficiente!")
+
+ 
