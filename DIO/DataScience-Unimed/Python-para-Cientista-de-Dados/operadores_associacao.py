6e51472f8897382871ce51d4936bd3b1de411c22
saulpaiva <saulpaiva@gmail.com>
2022-10-28 21:29:31 -0300

    update

---
+++ DIO/DataScience-Unimed/Python-para-Cientista-de-Dados/operadores_associacao.py
@@ -0,0 +1,10 @@
+frutas = ["Laranja", "uva", "limao"]
+curso = "Curso de Python"
+
+# Case sensitive
+print("laranja" in frutas)
+print("Laranja" in frutas)
+
+print("limao" not in frutas)
+
+print("Python" in curso)
