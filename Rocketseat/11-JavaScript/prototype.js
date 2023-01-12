238698f274ab5c3824c3f8c39020ef839ed33a67
saulpaiva <saulpaiva@gmail.com>
2023-01-12 11:06:56 -0300

    update

---
+++ Rocketseat/11-JavaScript/prototype.js
@@ -0,0 +1,30 @@
+// MANIPULANDO DADOS
+
+
+// // prototype
+
+// 'anyString'.__proto__
+
+// 23.0.__proto__
+
+// true.__proto__
+
+// // exemple to run in the console
+
+// ["a", "b"].splice()
+
+
+// // type conversion (typecasting) vs type coersion
+
+// console.log('9' + 5) // coersion of the 5 to string
+
+// console.log(Number('9') + 5) // conversion
+
+
+// // strings and numbers
+
+// let string = "123"
+// console.log(Number(string))
+
+// let number = 321
+// console.log(String(number))
