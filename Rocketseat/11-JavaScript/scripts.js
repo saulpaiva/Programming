9e88e0387ca42d58f9ea5bd82a40b6c115d31b61
saulpaiva <saulpaiva@gmail.com>
2023-01-11 21:23:05 -0300

    update

---
+++ Rocketseat/11-JavaScript/scripts.js
@@ -0,0 +1,15 @@
+// criar um aplicativo de frases motivacionais
+
+// declaration or function statement
+function createPhrases() {
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+// execute, run, call, invoke the function
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('Fim do programa')
+
