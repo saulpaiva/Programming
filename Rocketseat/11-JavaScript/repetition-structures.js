aa8562402626292814a73b96d1ef6ea3a710ac21
saulpaiva <saulpaiva@gmail.com>
2023-01-13 11:47:38 -0300

    update

---
+++ Rocketseat/11-JavaScript/repetition-structures.js
@@ -0,0 +1,56 @@
+// // Repetition structures
+
+
+// // for
+
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// for(let i = 10; i > 0; i--) {
+//     if(i === 5) {
+//         continue
+//     }
+//     if(i === 2) {
+//         break
+//     }
+//     console.log(i)
+// }
+
+
+// // while
+
+// let i = 100000000;
+
+// while(i > 1) {
+//     console.log(i)
+//     i /= 5;
+// }
+
+
+// // for...of
+
+// let name = 'Saul'
+// let names = ['João', 'Paulo', 'Pedro']
+
+// for (let char of name) {
+//     console.log(char)
+// }
+
+// for (let people of names) {
+//     console.log(people)
+// }
+
+
+// // for...in
+
+// let person = {
+//     name: 'Saul',
+//     age: 24,
+//     weight: 74.8,
+// }
+
+// for (let property in person) {
+//     console.log(property)
+//     console.log(person[property])
+// }
