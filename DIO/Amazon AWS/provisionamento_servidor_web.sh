0c7efb0efb223c88c0322371ed1d5c72e289cafc
saulpaiva <saulpaiva@gmail.com>
2023-03-07 15:06:51 -0300

    update

---
+++ DIO/Amazon AWS/provisionamento_servidor_web.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Updatin the server..."
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Downloading the application files..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd linux-site-dio-main
+cp -R * /var/www/html
