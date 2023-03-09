e70cd106b3890221568764fff33d3b624045b62d
saulpaiva <saulpaiva@gmail.com>
2023-03-09 17:04:57 -0300

    update

---
+++ Rocketseat/Sandbox/NodeJS/process.js
@@ -0,0 +1,13 @@
+// console.log(process)
+// console.log(process.argv)
+
+// console.log('seu nome é ', process.argv[2] + ' ' + process.argv[3])
+
+// const firstName = process.argv[2]
+// const lastName = process.argv[3]
+
+// console.log(`Seu nome é ${firstName} ${lastName}`)
+
+
+console.log(process.argv)
+// node process --name "Saul" --greeting "Tudo bem com você?"
