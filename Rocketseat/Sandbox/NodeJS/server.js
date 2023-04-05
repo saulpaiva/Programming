451c4df4c9c15194bf802d627ae1434166c9f799
saulpaiva <saulpaiva@gmail.com>
2023-04-05 17:15:09 -0300

    update

--- Rocketseat/Sandbox/NodeJS/server.js
+++ Rocketseat/Sandbox/NodeJS/server.js
@@ -4,12 +4,12 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res){
-    res.render("index");
+    res.render("pages/index");
 })
 
 app.get("/sobre", function (req, res){
-    res.render("about");
+    res.render("pages/about");
 })
 
 app.listen(8080);
-console.log("Servidor funcionando");
+console.log("Servidor funcionando...");
